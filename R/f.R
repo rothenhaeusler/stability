a6d251ff08a390c5f6269dcdc6d776836c6abe7f
Dominik <drothenhausler@gmail.com>
2022-04-20 14:38:00 -0700

    first commit

---
+++ R/f.R
@@ -0,0 +1,46 @@
+#' Evaluation of model stability under distribution shift
+#' @param model An object as returned by lm or glm 
+#' @param param A coefficient in summary(model)
+#' @param E A subset of predictors in the formula of the model
+#' @examples
+#' fit <- lm(sr ~ pop15 + pop75 + dpi + ddpi,
+#' data = LifeCycleSavings)
+#' stability(fit,param="pop15",E=c("pop75","dpi","pop15"))
+#' stability(fit,param="pop15")
+#'
+#' fit <- glm(Postwt ~ Prewt + Treat,family = gaussian, data = anorexia)
+#' stability(fit,param = "TreatCont")
+#'
+#' @export
+stability <- function(model,param,E=NULL) {
+  infl <- influence(fit)
+  influence_values <- sqrt(n)*infl$coefficients[,param]
+  n <- length(fit$fitted.values)
+  target_coef <- coef(fit)[param]
+  
+  
+  x <- seq(0,2,length.out=100)
+  main = paste("Stability of parameter",param)
+  main
+  plot(x,type = "n",col = "red", xlab = "Distribution shift in KL divergence", ylab = "Range of parameter under distribution shift", main = main,xlim=c(0,2),ylim = c(min(0,2*target_coef),max(0,2*target_coef)))
+  
+  if (is.null(E)) { E <- names(fit$model)}
+  
+  for (j in 1:length(E)){
+    
+    if (is.numeric(fit$model[,E[j]])){
+      splines <- loess(influence_values~fit$model[,E[j]])
+      f_values <- splines$fitted
+    } else {
+      f_values <- fitted.values(lm(influence_values ~ fit$model[,E[j]]))
+    }
+    
+    
+    y <- t(rbind(target_coef - 2*sqrt(x)*sd(f_values),target_coef + 2*sqrt(x)*sd(f_values)))
+    lines(cbind(x,y[,1]), type = "l", col = j)
+    lines(cbind(x,y[,2]), type = "l", col = j)
+    
+  }
+  legend(2,max(0,2*target_coef),xjust=1,pch=19,legend=E,col=1:length(E), title="Shift of ...")
+  
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+
+# R-package
+
+This package provides a function for evaluating the stability of lm and glm models under distribution shift.
+
+## How to install
+
+1. The [devtools](https://github.com/hadley/devtools) package has to be installed. You can install it using  `install.packages("devtools")`.
+2. The latest development version can then be installied using `devtools::install_github("rothenhaeusler/stability_values")`.
+
+## Usage
+
+The following code plots the distributional stability of the coefficient "pop15" under distributional shifts in "pop75", "dpi", and "pop15", respectively.
+```R
+fit <- lm(sr ~ pop15 + pop75 + dpi + ddpi, data = LifeCycleSavings)
+stability(fit,param="pop15",E=c("pop75","dpi","pop15"))
+```
+If the distribution shift is not specified, the function will evaluate the stability under shifts in all covariates.
+```R
+stability(fit,param="pop15")
+```
+The function can also be used for generalized linear models.
+```R
+fit <- glm(Postwt ~ Prewt + Treat,family = gaussian, data = anorexia)
+stability(fit,param = "TreatCont")
+```
+For discrete random variables, there are several parameters that might be of interest. As "parameter" one has to choose the parameter of interest using the dummified notation in summary(model).
